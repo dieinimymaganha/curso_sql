9d9085041a845ee153350a804afcfac1c04fa231
Dieinimy Maganha <dieinimy@gmail.com>
2019-07-22 16:32:14 -0300

    Resolvendo o ultimo exercicio funcionarios

--- Exercicio_Funcionarios.sql
+++ Exercicio_Funcionarios.sql
@@ -27,7 +27,7 @@ SELECT DEPARTAMENTO, COUNT(*) FROM FUNCIONARIOS GROUP BY DEPARTAMENTO;
 SELECT NOME FROM FUNCIONARIOS WHERE DEPARTAMENTO = 'ROUPAS' OR DEPARTAMENTO = 'FILMES';
 
 -- O gestor de marketing pediu a lista das funcionarias que trabalhem no departamento
--- de filmes ou no departamento lar. El necessita enviar um email para as colaborados
+-- de filmes ou no departamento lar. ElE necessita enviar um email para as colaborados
 -- desses dois setores.
 
 SELECT SEXO, COUNT(*) FROM FUNCIONARIOS 
@@ -38,4 +38,14 @@ GROUP BY SEXO;
 SELECT DEPARTAMENTO, COUNT(*) FROM FUNCIONARIOS
 GROUP BY DEPARTAMENTO;
 
--- FILMES 21 LAR 52
+-- FILMES 21 LAR 52, SEGUINDO A MESMA LOGICA ANTERIOR TEREMOS
+
+SELECT NOME, EMAIL FROM FUNCIONARIOS WHERE SEXO = 'FEMININO' AND (DEPARTAMENTO = 'LAR' OR DEPARTAMENTO = 'FILMES');
+
+-- Traga os funcionarios do sexo masculino 
+-- ou os funcionaros que trabalhem no setor Jardim
+
+
+-- Já que a quantidade de sexo masculino é de 484 não resta duvida sobre quem vem primeiro
+
+SELECT NOME, SEXO, DEPARTAMENTO FROM FUNCIONARIOS WHERE SEXO = 'MASCULINO' OR DEPARTAMENTO = 'JARDIM';
