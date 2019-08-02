a45b4d262bd0b4f340b24ffbe8acd3658c22b983
Dieinimy Maganha <dieinimy@gmail.com>
2019-08-02 13:24:21 -0300

    AULA 39 VERIFICANDO E ALTERANDO A ESTRUTURA DE UMA TABELA

--- Scripts-Versao-2019.sql
+++ Scripts-Versao-2019.sql
@@ -2895,3 +2895,127 @@ FROM VENDEDORES;
 | JOAO     |  12447.78 |  23265.87 |  96588.90 |  132302.55 |  33075.64 |  44100.84 |
 | CELIA    | 265767.78 | 989666.88 |  32655.90 | 1288090.56 | 322022.64 | 429363.51 |
 +----------+-----------+-----------+-----------+------------+-----------+-----------+
+
+/* AULA 39 VERIFICANDO E ALTERANDO A ESTRUTURA DE UMA TABELA */
+
+CREATE TABLE TABELA(
+	COLUNA1 VARCHAR(30),
+	COLUNA2 VARCHAR(30),
+	COLUNA3 VARCHAR(30)
+);
+
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| COLUNA1 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA2 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3 | varchar(30) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+
+-- INSERINDO PRIMARY KEY
+
+ALTER TABLE TABELA
+ADD PRIMARY KEY (COLUNA1);
+
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| COLUNA1 | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3 | varchar(30) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+
+
+-- ADICIONANDO COLUNA SEM POSICAO. ULTIMA POSIÇÃO
+
+ALTER TABLE TABELA
+ADD COLUNA VARCHAR(30);
+
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| COLUNA1 | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3 | varchar(30) | YES  |     | NULL    |       |
+| COLUNA  | varchar(30) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+
+ALTER TABLE TABELA
+ADD COLUNA100 INT(11);
+
++-----------+-------------+------+-----+---------+-------+
+| Field     | Type        | Null | Key | Default | Extra |
++-----------+-------------+------+-----+---------+-------+
+| COLUNA1   | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2   | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3   | varchar(30) | YES  |     | NULL    |       |
+| COLUNA    | varchar(30) | YES  |     | NULL    |       |
+| COLUNA100 | int(11)     | YES  |     | NULL    |       |
++-----------+-------------+------+-----+---------+-------+
+
+-- ADICIONANDO UMA COLUNA COM POSIÇÃO
+
+ALTER TABLE TABELA
+ADD COLUMN COLUNA4 VARCHAR(30) NOT NULL UNIQUE
+AFTER COLUNA3;
+
++-----------+-------------+------+-----+---------+-------+
+| Field     | Type        | Null | Key | Default | Extra |
++-----------+-------------+------+-----+---------+-------+
+| COLUNA1   | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2   | varchar(30) | YES  |     | NULL    |       |
+| COLUNA3   | varchar(30) | YES  |     | NULL    |       |
+| COLUNA4   | varchar(30) | NO   | UNI | NULL    |       |
+| COLUNA    | varchar(30) | YES  |     | NULL    |       |
+| COLUNA100 | int(11)     | YES  |     | NULL    |       |
++-----------+-------------+------+-----+---------+-------+
+
+-- MODIFICANDO O TIPO DE UM CAMPO
+
+ALTER TABLE TABELA MODIFY COLUNA2 DATE NOT NULL;
+
++-----------+-------------+------+-----+---------+-------+
+| Field     | Type        | Null | Key | Default | Extra |
++-----------+-------------+------+-----+---------+-------+
+| COLUNA1   | varchar(30) | NO   | PRI | NULL    |       |
+| COLUNA2   | date        | NO   |     | NULL    |       |
+| COLUNA3   | varchar(30) | YES  |     | NULL    |       |
+| COLUNA4   | varchar(30) | NO   | UNI | NULL    |       |
+| COLUNA    | varchar(30) | YES  |     | NULL    |       |
+| COLUNA100 | int(11)     | YES  |     | NULL    |       |
++-----------+-------------+------+-----+---------+-------+
+
+-- RENOMEANDO O NOME DA TABELA
+
+ALTER TABLE TABELA
+RENAME PESSOA;
+
+
+CREATE TABLE TIME(
+	IDTIME INT PRIMARY KEY AUTO_INCREMENT,
+	TIME VARCHAR(30),
+	ID_PESSOA VARCHAR(30)
+);
+
++-----------+-------------+------+-----+---------+----------------+
+| Field     | Type        | Null | Key | Default | Extra          |
++-----------+-------------+------+-----+---------+----------------+
+| IDTIME    | int(11)     | NO   | PRI | NULL    | auto_increment |
+| TIME      | varchar(30) | YES  |     | NULL    |                |
+| ID_PESSOA | varchar(30) | YES  |     | NULL    |                |
++-----------+-------------+------+-----+---------+----------------+
+-- FOREIGN KEY
+
+ALTER TABLE TIME
+ADD FOREIGN KEY (ID_PESSOA)
+REFERENCES PESSOA(COLUNA1);
+
+
+/* VERIFICAR AS CHAVES */
++-----------+-------------+------+-----+---------+----------------+
+| Field     | Type        | Null | Key | Default | Extra          |
++-----------+-------------+------+-----+---------+----------------+
+| IDTIME    | int(11)     | NO   | PRI | NULL    | auto_increment |
+| TIME      | varchar(30) | YES  |     | NULL    |                |
+| ID_PESSOA | varchar(30) | YES  | MUL | NULL    |                |
++-----------+-------------+------+-----+---------+----------------+
